778a260b8d754fa3217566992de5cff4abfa0a28
icepine.hans <icepine.hans@antfin.com>
2018-09-27 21:20:22 +0800

    refactor

--- my_library/models/reading_comprehension/__init__.py
+++ my_library/models/reading_comprehension/__init__.py
@@ -1,4 +1,3 @@
 from my_library.models.reading_comprehension.rnet import *
-from my_library.models.reading_comprehension.multi_granu_hierarch_attention_elmo import MultiGranuFusionElmo
 from my_library.models.reading_comprehension.multi_granu_hierarch_attention_fusion import MultiGranuFusion
 from my_library.models.reading_comprehension.rnet_ensemble import RNetEnsemble
