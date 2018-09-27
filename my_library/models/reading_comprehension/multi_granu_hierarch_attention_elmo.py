33bd9c7eb044bc188860189428fceaf478caef1c
icepine.hans <icepine.hans@antfin.com>
2018-09-27 14:25:17 +0800

    restore phrase_layer

--- my_library/models/reading_comprehension/multi_granu_hierarch_attention_elmo.py
+++ my_library/models/reading_comprehension/multi_granu_hierarch_attention_elmo.py
@@ -71,7 +71,7 @@ class MultiGranuFusionElmo(Model):
 	def __init__(self, vocab: Vocabulary,
 				 text_field_embedder: TextFieldEmbedder,
 				 num_highway_layers: int,
-				 elmo_layer: Seq2SeqEncoder,
+				 phrase_layer: Seq2SeqEncoder,
 				 soft_align_matrix_attention: SoftAlignmentMatrixAttention,
 				 self_matrix_attention: BilinearMatrixAttention,
 				 passage_modeling_layer: Seq2SeqEncoder,
@@ -86,10 +86,10 @@ class MultiGranuFusionElmo(Model):
 		super(MultiGranuFusionElmo, self).__init__(vocab, regularizer)
 
 		self._text_field_embedder = text_field_embedder
-		# self._highway_layer = TimeDistributed(Highway(text_field_embedder.get_output_dim(),
-		# 											  num_highway_layers))
-		self._elmo_layer = elmo_layer
-		self._elmo_layer = _Seq2SeqWrapper(AlternatingHighwayLSTM)(input_size=1224, hidden_size=200)
+		self._highway_layer = TimeDistributed(Highway(text_field_embedder.get_output_dim(),
+													  num_highway_layers))
+		self._phrase_layer = phrase_layer
+		# self._elmo_layer = _Seq2SeqWrapper(AlternatingHighwayLSTM)(input_size=1224, hidden_size=200)
 		self._matrix_attention = soft_align_matrix_attention
 		self._self_matrix_attention = self_matrix_attention
 		self._passage_modeling_layer = passage_modeling_layer
@@ -101,7 +101,7 @@ class MultiGranuFusionElmo(Model):
 		passage_modeling_output_dim = self._passage_modeling_layer.get_output_dim()
 		question_modeling_output_dim = self._question_modeling_layer.get_output_dim()
 
-		encoding_dim = elmo_layer.get_output_dim()
+		encoding_dim = phrase_layer.get_output_dim()
 		self._passage_fusion_weight = nn.Linear(encoding_dim * 4, encoding_dim)
 		self._question_fusion_weight = nn.Linear(encoding_dim * 4, encoding_dim)
 		self._fusion_weight = nn.Linear(encoding_dim * 4, encoding_dim)
@@ -204,10 +204,8 @@ class MultiGranuFusionElmo(Model):
 			string from the original passage that the model thinks is the best answer to the
 			question.
 		"""
-		# embedded_question = self._highway_layer(self._text_field_embedder(question))
-		# embedded_passage = self._highway_layer(self._text_field_embedder(passage))
-		embedded_question = self._text_field_embedder(question)
-		embedded_passage = self._text_field_embedder(passage)
+		embedded_question = self._highway_layer(self._text_field_embedder(question))
+		embedded_passage = self._highway_layer(self._text_field_embedder(passage))
 		batch_size = embedded_question.size(0)
 		passage_length = embedded_passage.size(1)
 		question_mask = util.get_text_field_mask(question).float()
@@ -215,10 +213,8 @@ class MultiGranuFusionElmo(Model):
 		question_lstm_mask = question_mask if self._mask_lstms else None
 		passage_lstm_mask = passage_mask if self._mask_lstms else None
 
-		# encoded_question = self._dropout(self._phrase_layer(embedded_question, question_lstm_mask))
-		# encoded_passage = self._dropout(self._phrase_layer(embedded_passage, passage_lstm_mask))
-		encoded_question = self._dropout(self._elmo_layer(embedded_question, question_lstm_mask))
-		encoded_passage = self._dropout(self._elmo_layer(embedded_passage, passage_lstm_mask))
+		encoded_question = self._dropout(self._phrase_layer(embedded_question, question_lstm_mask))
+		encoded_passage = self._dropout(self._phrase_layer(embedded_passage, passage_lstm_mask))
 		encoding_dim = encoded_question.size(-1)
 
 		# Shape: (batch_size, passage_length, question_length)
