f3c59b0920fc312eae29e7d00e4ac6eb15224f87
乔公 <icepine.hans@antfin.com>
2019-02-28 11:25:02 +0800

    add more predictors

--- my_library/predictors/joint_intent_slot_predictor.py
+++ my_library/predictors/joint_intent_slot_predictor.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-
+import json
 from overrides import overrides
 
 from allennlp.common.util import JsonDict
@@ -25,9 +25,25 @@ class JointIntentSlotPredictorPlain(Predictor):
     """"Predictor wrapper for the AcademicPaperClassifier"""
 
     @overrides
+    def load_line(self, line: str) -> JsonDict:
+        line = line.strip()
+        if line == '':
+            raise Exception('empty query to predict')
+        return {'query': line}
+
+    @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
-        print('----------->')
         query = json_dict['query']
         instance = self._dataset_reader.text_to_instance(
             query, tags=None, label=None)
         return instance
+
+    @overrides
+    def dump_line(self, outputs: JsonDict) -> str:  # pylint: disable=no-self-use
+        """
+        If you don't want your outputs in JSON-lines format
+        you can override this function to output them differently.
+        """
+        first_intent_score = outputs['top 3 intents'][0]
+        first_intent = first_intent_score.split(':')[0]
+        return first_intent + "\n"
